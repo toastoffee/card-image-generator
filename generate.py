3ea50068ddda594ae9ea040daa1c84a6319623c3
toastoffee <931391442@qq.com>
2025-06-07 15:39:35 +0800

    saminuosi

---
+++ generate.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+
+
+def draw_text_multiline(draw, xy, text, font, line_len, line_height):
+    i = 0
+    while i < len(text):
+        draw.text(xy, text[i:i + line_len], font=font, fill='white')
+        i += line_len
+        xy = (xy[0], xy[1] + line_height)
+
+CARD_IMAGE_PATH = "card.png"
+
+if __name__ == "__main__":
+
+    card_data = pd.read_excel('card_data.xlsx', sheet_name='Sheet1')
+    print(card_data.values)
+
+    for entity in card_data.values:
+
+        width, height = 700, 1200
+        # image = Image.new('RGB', (width, height), color='white')
+        image = Image.open(CARD_IMAGE_PATH)
+
+        title_font = ImageFont.truetype('Songti.ttc', 100)
+        desc_font = ImageFont.truetype('Songti.ttc', 50)
+
+        title_x, title_y = 50, 60
+        desc_x, desc_y = 50, 250
+
+        draw = ImageDraw.Draw(image)
+
+        # draw title
+        draw.text((title_x, title_y), entity[0], font=title_font, fill='white')
+
+        # draw desc
+        draw_text_multiline(draw, (desc_x, desc_y), entity[1], desc_font, 12, 65)
+
+        image.save(f'exports/{entity[0]}.png')
